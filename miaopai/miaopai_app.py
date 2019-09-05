82ce3cb669c674a7714fa52dd549e48b7937b6a5
Leo <imwoolei@outlook.com>
2019-09-05 10:22:38 +0800

    add 新增美拍APP爬虫demo delete 删除起点中文网脚本

--- miaopai/miaopai_app.py
+++ miaopai/miaopai_app.py
@@ -31,32 +31,47 @@ class MiaopaiApp:
     # 生成cp_sign的Key，定值
     const_key = '4O230P1eeOixfktCk2B0K8d0PcjyPoBC'
     # 视频信息获取url，其中smid即为视频id
-    info_url = 'http://b-api.ins.miaopai.com/1/recommend/related.json?smid={smid}'
+    info_url = 'http://b-api.ins.miaopai.com/1/media/info.json?smid={smid}'
     # 评论列表获取url，其中smid即为视频id，count请求的评论条数，page页码数
     comment_url = 'http://b-api.ins.miaopai.com/2/comment/list.json?count={count}&page={page}&smid={smid}'
 
+    # 请求头必需参数
+    base_headers = {'cp_uuid': '', 'cp_ver': '', 'cp_sign': '', 'cp_time': '', 'Connection': 'close',
+                    'Host': 'b-api.ins.miaopai.com', 'User-Agent': 'okhttp/3.3.1', 'cp_os': 'android',
+                    'cp_channel': 'ppzhushou_market'}
+
     def __init__(self, video_id: str):
         self.video_id = video_id
-        # 请求头必需参数
-        self.headers = {'cp_uuid': '', 'cp_ver': '', 'cp_sign': '', 'cp_time': '', 'Connection': 'close',
-                        'Host': 'b-api.ins.miaopai.com', 'User-Agent': 'okhttp/3.3.1', 'cp_os': 'android',
-                        'cp_channel': 'ppzhushou_market'}
+        # # 请求头必需参数
+        # self.headers = {'cp_uuid': '', 'cp_ver': '', 'cp_sign': '', 'cp_time': '', 'Connection': 'close',
+        #                 'Host': 'b-api.ins.miaopai.com', 'User-Agent': 'okhttp/3.3.1', 'cp_os': 'android',
+        #                 'cp_channel': 'ppzhushou_market'}
 
     def get_video_info(self):
         """获取视频信息"""
         full_info_url = self.info_url.format(smid=self.video_id)
-        self._set_headers(target_url=full_info_url)
-        resp = requests.get(url=full_info_url, headers=self.headers)
+        headers = self._get_headers(target_url=full_info_url)
+        resp = requests.get(url=full_info_url, headers=headers)
         if not resp.ok:
-            raise requests.exceptions.RequestException('视频信息响应状态码异常, status_code: %s' % resp.status_code)
+            raise requests.exceptions.RequestException('请求视频信息响应状态码异常, status_code: %s' % resp.status_code)
         resp_text = self._decode_resp_content(resp_content=resp.content)
-        print(resp_text)
+        video_info = json.loads(resp_text.strip('0'))
+        return video_info
 
-    def get_video_comments(self):
-        """获取视频评论"""
-        pass
+    def get_video_comments(self, count=20, page=1):
+        """获取视频评论
+        :param count: 单页请求评论数
+        :param page: 请求页码数"""
+        full_comment_url = self.comment_url.format(count=count, page=page, smid=self.video_id)
+        headers = self._get_headers(target_url=full_comment_url)
+        resp = requests.get(url=full_comment_url, headers=headers)
+        if not resp.ok:
+            raise requests.exceptions.RequestException('请求视频评论列表响应状态码异常, status_code: %s' % resp.status_code)
+        resp_text = self._decode_resp_content(resp_content=resp.content)
+        video_comments = json.loads(resp_text.strip('0'))
+        return video_comments
 
-    def _set_headers(self, target_url: str) -> None:
+    def _get_headers(self, target_url: str) -> dict:
         """设置请求headers
         cp_sign由URL路径、uuid、版本号、时间戳以及定值字符串key拼接后由md5加密生成
         :param target_url: 需要请求的目标URL
@@ -74,10 +89,12 @@ class MiaopaiApp:
 
         current_ts = str(int(time.time()))
         fake_uuid = str(uuid.uuid1())  # 伪造UUID，也叫做GUID(C#)
-        self.headers['cp_ver'] = self.app_version
-        self.headers['cp_time'] = current_ts
-        self.headers['cp_uuid'] = fake_uuid
-        self.headers['cp_sign'] = get_cp_sign()
+        cur_headers = self.base_headers.copy()
+        cur_headers['cp_ver'] = self.app_version
+        cur_headers['cp_time'] = current_ts
+        cur_headers['cp_uuid'] = fake_uuid
+        cur_headers['cp_sign'] = get_cp_sign()
+        return cur_headers
 
     @staticmethod
     def _decode_resp_content(resp_content):
@@ -110,7 +127,9 @@ class MiaopaiApp:
 
 
 if __name__ == '__main__':
-    test_video_id = 'C1U6Bq~RCnhkLJSsbwxxx1~B4zfWDI4d'
+    test_video_id = 'sg7nsSlTA27Vjf4Z-BPvCccsUloQyWce'
     spider = MiaopaiApp(video_id=test_video_id)
-    spider.get_video_info()
-    # spider.get_video_comments()
+    info = spider.get_video_info()
+    print('视频信息->', json.dumps(info, ensure_ascii=False))
+    comments = spider.get_video_comments()
+    print('评论列表->', json.dumps(comments, ensure_ascii=False))
